ec439c0c1adfde448b707107ca7173d5e866d327
007MJ <jetforce@jetforces-MacBook-Pro.local>
2024-10-01 22:30:33 -0400

    magic number game done

--- tron_commun/base/turtue/main.py
+++ tron_commun/base/turtue/main.py
@@ -1,5 +1,20 @@
 import turtle
 
+
+def carre(size):
+    for i in range(0, 4):
+         t.forward(size)
+         t.left(90)
+
+    
+def carreN(size, nb):
+    if nb == 0:
+        nb =1
+    for i in range(0, nb):
+        n_size = size * (i + 1)
+        carre(n_size)
+
+
 def stare(n, size=30):
     for i in range(0, n):
         t.forward(size)
@@ -7,8 +22,11 @@ def stare(n, size=30):
         t.forward(size)
         t.right(90)
         i +=1
-    t.forward(30)
+        size +=10
+    t.forward(size)
 
 t = turtle.Turtle()
-stare(2)
+# stare(2)
+
+carreN(50, 3)
 turtle.done()
