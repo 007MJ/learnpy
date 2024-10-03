43397e753caaf3a979253f18e9b8fd8d956d1736
007MJ <jetforce@jetforces-MacBook-Pro.local>
2024-10-03 00:12:45 -0400

    remove the tron folder

---
+++ base/questionnaire/main.py
@@ -0,0 +1,12 @@
+def ask_questionn(question, a, b, c, d):
+    print(F"{question} ?")
+    print(f"(a) {a}")
+    print(f"(b) {b}")
+    print(f"(c) {c}")
+    print(f"(d) {d}")
+    usr_input = input("tha is the answer :")
+    return (usr_input)
+
+
+
+ask_questionn("Who is the best player of basket-ball", "me", "you", "Michel", "your DAD")
