bf24cb9a6baa28e2777a331857ca67f40fc793c8
007MJ <jetforce@jetforces-MacBook-Pro.local>
2024-10-05 17:27:56 -0400

    parsing end

--- base/calculatrice/main.py
+++ base/calculatrice/main.py
@@ -1,30 +1,61 @@
-def multi(nb1, nb2) :
-    return (nb1 * nb1)
+def digit_number(str):
+    for i in str:
+        if i != '*' or i != '+' or i != '-' or i != '/':
+            if (i.isalpha() == True):
+                print("digit and signe only")
+                return ""
+    return str
 
-def addition(nb1, nb2) :
-    return (nb1 + nb1)
+def signe(str):
+    _signe = False
+    for i in range(len(str) -1):
+        _signe = False
+        if str[i] == '*' or  str[i] == '-' or str[i] == '+' or str[i] == '/':
+            if i > len(str) -1:
+                if (str[i - 1].isspace()) == False:
+                    print("space bewteen *")
+                    return ""
+            while (str[i].isspace()) == False and i < len(str) -1:
+                    if (str[i] == '*' or  str[i] == '-' or str[i] == '+' or str[i] == '/') and _signe == False:
+                        _signe = True
+                    else:
+                        print("To many signe")
+                        return ""
+                    i +=1
 
-def soustraction(nb1, nb2) :
-    return (nb1 - nb1)
-
-
-def divisio(nb1, nb2) :
-    return (nb1 / nb1)
-
+            _signe = False
+            if i < len(str) -1:
+                if (str[i + 1].isdigit()) == False:
+                    print("is number", str[i])
+                    print("Bad use of signe")
+                    return ""
+    return str
+        
 
 def take_number():
     str = ""
     while str == "":
         str = input("Enter :")
-        for i in str:
-            if i == 32 or i.isdigit():
-                return str
+        if len(str) == 1:
+            if str[0].isdigit() :
+                return (str)
             else:
                 str = ""
-                print("NUmbers  and operators * / + -")
-                break;
+                continue
+        str = digit_number(str)
+        str = signe(str)
+        for index in range (len(str) - 1):
+            if str[index].isdigit():
+                if index < len(str):
+                    if (str[index + 1].isdigit() == False) and (str[index + 1].isspace()) == False:
+                        str = ""
+                        print("Space bewteen signe and number")
+                        break;
+
 
     return (str);  
+
+
 
 
 usr_numbers = take_number()
