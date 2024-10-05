6c3fb3a79fda336c07d56c1e8389b730a2eb2856
007MJ <jetforce@jetforces-MacBook-Pro.local>
2024-10-05 19:01:55 -0400

    test

--- exercice/polynôme.py
+++
@@ -1,4 +0,0 @@
-str = input("Enter an integer : ")
-
-result = 3 * (int(str) * int(str)) + 5 * int(str) + 1
-print("Resultat polynome 3{str}² + 5{str} + 1 = ", result)
--- exercice/rev_print.py
+++
@@ -1,8 +0,0 @@
-def rev_print(str):
-    l = len(str) - 1
-    while (l >= 0):
-        print(str[l], end='')
-        l -=1
-
-
-rev_print("bonjour")
